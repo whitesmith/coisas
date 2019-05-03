e51778baaadee9484a293aee4dc5c13c2a8227db
Tomás Correia Marques <tomasmanuelcorreiamarques@gmail.com>
2019-05-03 16:19:41 +0100

    add gh-pages deploy

--- src/app.js
+++ src/app.js
@@ -34,7 +34,7 @@ render(
 )
 
 if ('serviceWorker' in navigator) {
-  navigator.serviceWorker.register('/service-worker.js', {scope: '/'})
+  navigator.serviceWorker.register('../service-worker.js', {scope: '/'})
     .then(reg => console.log('service worker registered.', reg.scope))
     .catch(e => console.log('failed to register service worker.', e))
 }
